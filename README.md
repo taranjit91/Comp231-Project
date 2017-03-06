cf68c414f3d24f4ec3723c30cc171ee7e218bdf1
= <jishnusundar423@gmail.com>
2017-03-06 15:47:19 -0500

    Created Website Startup Framewrok

---
+++ README.md
@@ -0,0 +1,5 @@
+# COMP308-W2017-Lesson6A
+
+## Welcome to Lesson 6 - Authentication with passport
+
+please use **`npm install`** to install project dependencies
---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+
+//modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+let passport = require('passport');
+
+
+module.exports = router;
